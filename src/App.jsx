94396c40fb53990781b5a7671926e40807e4a642
student <jakub.gola2@teb.edu.pl>
2025-02-21 12:20:32 +0100

    21.02.2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+import profiles from './assets/profile.json';
+import messages from './assets/wiadomosci.json';
+import { createId } from '@paralleldrive/cuid2';
+
+
+// id, data, twórca, tytuł, treść
+class Post{
+  constructor(author, title, content){
+    this.author = author;
+    this.title = title;
+    this.content = content;
+    this.id = createId();
+    this.date = new Date().toLocaleString("pl-pl")
+  }
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+  function generatePost() {
+    const profileIndex = Math.floor(Math.random() * profiles.length)
+    const profile = profiles[profileIndex]
+
+    const messageIndex = Math.floor(Math.random() * messages.length)
+    const message = messages[messageIndex]
+
+    const post = new Post(profile.nazwa, message.tytul, message.tresc);
+
+    setPosts([post, ...posts]);
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(generatePost, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [generatePost]);
+
+
+
+  const postsToDisplay = posts.map(p => <div key={p.id}>
+    <div className="card text-center">
+      <div className="card-header">
+        {p.author}
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{p.title}</h5>
+        <p className="card-text">{p.content}</p>
+      </div>
+      <div className="card-footer text-body-secondary">
+        Date: {p.date}
+      </div>
+    </div>
+  </div>)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container-fluid">
+        <button onClick={generatePost}>Wygeneruj post</button>
+        {postsToDisplay}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
